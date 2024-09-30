7879f2ca77a92ce94188b482b2d1e8dc86e1d2fe
Edras <monterrosoedras@gmail.com>
2024-09-29 20:06:33 -0600

    se agraron campos adicionales a la consulta y se quito la condicion

--- citasBack/scripts/sql/procedimientos/obtener_servicios.sql
+++ citasBack/scripts/sql/procedimientos/obtener_servicios.sql
@@ -4,10 +4,12 @@ DELIMITER $$
 CREATE PROCEDURE `obtener_servicios` () 
 BEGIN
 	SELECT id,
+		fecha_creacion,
+		fecha_actualizacion,
 		nombre,
 		precio,
 		descripcion,
+		privado,
 		duracion
-	FROM servicios
-	WHERE privado <> 1;
+	FROM servicios;
 END $$
