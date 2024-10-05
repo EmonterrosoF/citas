aa4a0e6f8c9e0b5c24bce6c9fd5aca5965d56d5f
Edras <monterrosoedras@gmail.com>
2024-10-04 22:43:07 -0600

    se agregaron nuevas variables

--- citasBack/config.js
+++ citasBack/config.js
@@ -6,15 +6,16 @@ config();
 export const PORT = process.env.PORT ?? 3000;
 
 // SECRETO PARA JSONWEBTOKEN
-export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_TOKEN_SECRET = process.env.JWT_TOKEN_SECRET;
+export const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN_SECRET;
 
-// CREDENCIALES PARA SQL SERVER
+// CREDENCIALES PARA MYSQL
 export const USER = process.env.USER;
 export const PASS = process.env.PASS;
 export const SERVER = process.env.SERVER;
 export const DB = process.env.DB;
 
-//CONEXION MONGO
-export const MONGO_URL = process.env.MONGO_URL;
-
 export const HOST_FRONT = process.env.HOST_FRONT ?? "http://localhost:5173";
+
+// api brevo para correo
+export const API_KEY_BREVO = process.env.API_KEY_BREVO;
