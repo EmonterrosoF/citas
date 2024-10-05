515f2bfdd0e0add9d145197d2e08c00ca5650e73
Edras <monterrosoedras@gmail.com>
2024-10-04 22:47:49 -0600

    se agregaron proteccion de rutas

--- citasFront/src/App.jsx
+++ citasFront/src/App.jsx
@@ -9,21 +9,52 @@ import ServiciosPage from "./pages/privadas/ServiciosPage";
 import UsuariosPage from "./pages/privadas/usuariosPage";
 import { PerfilPage } from "./pages/privadas/PerfilPage";
 import ConfigPage from "./pages/privadas/configPage";
+import LoginPage from "./pages/privadas/LoginPage";
+import RecuperarPage from "./pages/privadas/RecuperarPage";
+import { AuthProvider } from "./context/authProvider";
+import ProtectedRoute from "./components/protectedRoute";
+import VerificateIsLogin from "./components/verificateIsLogin";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/panel" element={<Layout />}>
-          <Route index element={<CalendarioPage />}></Route>
-          <Route path="clientes" element={<ClientesPage />}></Route>
-          <Route path="servicios" element={<ServiciosPage />}></Route>
-          <Route path="usuarios" element={<UsuariosPage />}></Route>
-          <Route path="perfil" element={<PerfilPage />}></Route>
-          <Route path="configuraciones" element={<ConfigPage />}></Route>
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route
+            path="/panel"
+            element={
+              <ProtectedRoute>
+                <Layout />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<CalendarioPage />}></Route>
+            <Route path="clientes" element={<ClientesPage />}></Route>
+            <Route path="servicios" element={<ServiciosPage />}></Route>
+            <Route path="usuarios" element={<UsuariosPage />}></Route>
+            <Route path="perfil" element={<PerfilPage />}></Route>
+            <Route path="configuraciones" element={<ConfigPage />}></Route>
+          </Route>
+          <Route
+            path="/login"
+            element={
+              <VerificateIsLogin>
+                <LoginPage />
+              </VerificateIsLogin>
+            }
+          />
+          <Route
+            path="/recuperar"
+            element={
+              <VerificateIsLogin>
+                <RecuperarPage />
+              </VerificateIsLogin>
+            }
+          />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
