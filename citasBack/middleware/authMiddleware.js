d601e99bde345976bca28e4aa86f22c7ecd3d02a
Edras <monterrosoedras@gmail.com>
2024-10-04 22:44:57 -0600

    se agrego middleware para autenticacion

--- citasBack/middleware/authMiddleware.js
+++ citasBack/middleware/authMiddleware.js
@@ -1,69 +1,69 @@
-// import jwt from "jsonwebtoken";
-// import { JWT_SECRET } from "../config.js";
-// import mssql from "mssql";
-// import { SP_LOGIN_USUARIO } from "../utils/sp.js";
+import jwt from "jsonwebtoken";
+
+import { JWT_REFRESH_TOKEN_SECRET } from "../config.js";
+import { ejecutarSP } from "../data/dbConexion.js";
+import { SP_PRE_AUTENTICAR } from "../utils/sp.js";
 
 // // middleware que me permite verificar que el usuario este logueado
-// export const estaLogueado = async (req, res, next) => {
-//   let token;
+export const estaLogueado = async (req, res, next) => {
+  try {
+    const { refreshToken } = req.cookies; // Obtener el refresh token de las cookies
+    console.log("refresh token: " + refreshToken);
+    if (!refreshToken) {
+      res.status(401);
+      const error = new Error("No existe el token");
+      return next(error);
+    }
 
-//   if (
-//     req.headers.authorization &&
-//     req.headers.authorization.startsWith("Bearer")
-//   ) {
-//     try {
-//       token = req.headers.authorization.split(" ")[1];
+    const user = jwt.verify(refreshToken, JWT_REFRESH_TOKEN_SECRET);
 
-//       const decoded = jwt.verify(token, JWT_SECRET);
+    if (!user) {
+      res.status(401);
+      const error = new Error("El token es invalido");
+      return next(error);
+    }
 
-//       const request = new mssql.Request();
+    const resultado = await ejecutarSP(SP_PRE_AUTENTICAR, [user.username]);
 
-//       request.input("correo", mssql.VarChar(30), decoded.correo);
+    if (resultado.length < 1) {
+      res.status(401);
+      const error = new Error("El token es invalido");
+      return next(error);
+    }
 
-//       request.execute(SP_LOGIN_USUARIO, async (err, result) => {
-//         if (err) {
-//           console.error(`Error en el sp ${SP_LOGIN_USUARIO}:`, err);
-//           const error = new Error("Error interno  del servidor");
-//           return next(error);
-//         }
-//         const resultado = result.recordset[0];
+    req.usuario = user;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
+    // Manejar errores específicos de JWT
+    if (err.name === "TokenExpiredError") {
+      res.status(401);
+      const error = new Error("El token ha expirado");
+      return next(error);
+    }
+    if (err.name === "JsonWebTokenError") {
+      res.status(401);
+      const error = new Error("Token no válido");
+      return next(error);
+    }
 
-//         console.log(resultado);
-//         if (resultado?.usuarioExistente === 0) {
-//           res.status(400);
-//           const error = new Error(
-//             "Usuario no encontrado, pero el token es válido"
-//           );
-//           res.clearCookie("token");
-//           return next(error);
-//         }
-//         req.usuario = resultado;
-//         next();
-//       });
-//     } catch (error) {
-//       console.error(error);
-//       res.status(401);
-//       console.log(error.message);
-//       const err = new Error("El token es inválido");
-//       res.clearCookie("token");
-//       next(err);
-//     }
-//   }
-//   if (!token) {
-//     res.status(401);
-//     const error = new Error("no existe el token");
-//     res.clearCookie("token");
-//     next(error);
-//   }
-// };
+    const error = new Error(
+      "Ha ocurrido un error, por favor intenta nuevamente o mas tarde"
+    );
+    next(error);
+  }
+};
 
 // // // middleware que me permite verificar que el usuario es admin
-// // export const admin = (req, res, next) => {
-// //   if (req.user && req.user.isAdmin) {
-// //     next();
-// //   } else {
-// //     res.status(401);
-// //     const error = new Error("Solo personal Autorizado");
-// //     next(error);
-// //   }
-// // };
+export const admin = (req, res, next) => {
+  const usuario = req.usuario;
+  if (usuario.rol && usuario.rol === "ADMIN") {
+    next();
+  } else {
+    res.status(401);
+    const error = new Error("Solo personal autorizado");
+    return next(error);
+  }
+};
